36e2447219bd676623f1437e6a982a6ebf6c16dd
Adrian Melo <amelo2@fvi.edu>
2017-04-12 21:26:34 -0400

    finish

--- js/main.js
+++ js/main.js
@@ -11,38 +11,64 @@ firebase.initializeApp(config);
 var db = firebase.database();
 var messagesRef = db.ref('messages/');
 
-var provider = new firebase.auth.FacebookAuthProvider();
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  var token = result.credential.accessToken;
-  var user = result.user;
-  $("#login-screen").fadeOut(function(){
-    $("#chat-screen").fadeIn();
-  });
-
-  messagesRef.on('value', function(snapshot){
-    //console.log(snapshot.val());
-    $("#messages").html("");
-    var values = snapshot.val();
-    for (var msgId in values){
-      var msg = values[msgId];
-      $("#messages").append(`
-        <li><strong>${msg.user}</strong>: ${msg.message}</li>
-        `);
-    }
-  });
-  document.addEventListener("keydown", function(e){
-    if (e.keyCode === 13) sendMessage(user.displayName);
-  });
-
-}).catch(function(error) {
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  console.log(errorCode + " - " + errorMessage);
-});
-
-function sendMessage(username){
-  messagesRef.push({
-    user: username,
-    message: $("#message-input").val()
-  });
+function sendMessage(user) {
+    messagesRef.push({
+        user: user.displayName,
+        message: $("#message-input").val(),
+        photo: user.photoURL
+    });
+    $("#message-input").val("");
+    window.scrollTo(0,document.body.scrollHeight);
 }
+
+function loadMessages() {
+    messagesRef.limitToLast(100).on('value', function(snapshot) {
+        //console.log(snapshot.val());
+        $("#messages").html("");
+        var values = snapshot.val();
+        for (var msgId in values) {
+            var msg = values[msgId];
+            $("#messages").append(`
+                <div class="message-container">
+                    <img class="user-photo" src=${msg.photo} alt=${msg.user} />
+                    <div class="text">
+                        <p class="user-name">${msg.user}</p>
+                        <p class="message">${msg.message}</p>
+                    </div>
+                </div>
+            `);
+        }
+    });
+}
+
+
+// DEVELOPMENT CODE; delete the following code for production
+loadMessages()
+// end development code
+
+
+function login() {
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        var token = result.credential.accessToken;
+        var user = result.user;
+
+       $('#login-screen').fadeOut(function() {
+            $("#chat-screen").fadeIn();
+        });
+
+       loadMessages()
+
+       document.addEventListener("keydown", function(e) {
+            if (e.keyCode === 13)
+                sendMessage(user);
+            }
+        );
+    }).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode + " - " + errorMessage);
+    });
+}
+
+$('#login-btn').click(login)
